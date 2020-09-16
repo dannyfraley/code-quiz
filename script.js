438ed81f364e5416ca01b37c2bc5d0e7bbe1bbe9
Danny Fraley <dannyfraley@gmail.com>
2020-09-15 22:47:36 -0400

    fixed questions

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 var questions = [
     {
-        ask: "How many licks does it take to get to the tootsie roll center of a tootsie pop?",
-        options: ["1", "2", "3", "4"],
-        answer: "3"
+        ask: "Inside which HTML element do we put the JavaScript?",
+        options: ["<scripting>", "<script>", "<js>", "<javascript>"],
+        answer: "<script>"
     },
     {
-        ask: "Who farted?",
-        options: ["You did.", "I did.", "He did.", "She did."],
-        answer: "I did."
+        ask: "Where is the correct place to insert a JavaScript?",
+        options: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body> section", "The <rhythm> section"],
+        answer: "Both the <head> section and the <body> section are correct"
     },
     {
-        ask: "Can I borrow some money?",
-        options: ["Yeah, how much do you need?", "Why do you need it?", "No fucking way.", "Sorry, I'm broke."],
-        answer: "No fucking way."
+        ask: "How does a while loop start?",
+        options: ["while (i <=10)", "while i = 1 to 10", "while (i <= 10; i++)", "while e. coyote"],
+        answer: "while (i <=10)"
     },
     {
-        ask: "Who shot JFK?",
-        options: ["Lee Harvey Oswald", "John Wilkes Booth", "James Earl Ray", "Meredith Baxter Birney"],
-        answer: "Lee Harvey Oswald"
+        ask: "How does a for loop start?",
+        options: ["for (i = 0; i <=5)", "for i = 1 to 5", "for (i = 0; i <=5; i++)", "for (i <= 5; i++)"],
+        answer: "for (i = 0; i <=5; i++)"
     },
     {
-        ask: "Why you gotta be like that?",
-        options: ["That's just how I am.", "Be like what?", "What the hell is that supposed to mean?", "Deal with it."],
-        answer: "Be like what?"
+        ask: "How can you add a comment in JavaScript?",
+        options: ["`This is a comment", "//This is a comment", "<!--This is a comment-->", "THIS IS A COMMENT!"],
+        answer: "//This is a comment"
+        
     }
 ];
 var quesCounter = 0;
 var score = 0;
 var time = 60;
-/*var timeDisplay = document.querySelector("#time");
-var timeHide = document.querySelector(".go-away");
-var timerText = document.querySelector("#time-style");*/
+var endGame = false;
+
 
 var startButton = document.querySelector("#start");
 var welcomeScreen = document.querySelector("#quizBody");
@@ -42,8 +42,13 @@ startButton.addEventListener("click", function () {
 });
 
 function loadQuiz(num){
+    if (num >= questions.length-1) {
+        endGame = true;
+        console.log(endGame);
+    }
     var p = document.createElement("h2");
     p.textContent = questions[num].ask;
+    document.querySelector("#questions").innerHTML = "";
     document.querySelector("#questions").append(p);
     for(var i=0;i<questions[num].options.length;i++){
         var answerButton = document.createElement("button");
@@ -54,6 +59,76 @@ function loadQuiz(num){
         answerButton.onclick = ansCheck;
     }
 }
+
+function ansCheck(){
+    var userAns = this.getAttribute("data-ans");
+    console.log("User Ans: "+userAns);
+    
+    
+    if(userAns === questions[quesCounter].answer){
+        score = score + 10;
+    }
+    else{
+        time = time - 5;
+       
+    }
+    console.log("endGame ", endGame)
+    if (endGame){
+        return endQuiz()
+    }
+    quesCounter++;
+    loadQuiz(quesCounter);
+}
+
+function endQuiz() {
+    document.querySelector("#questions").innerHTML = "";
+    var finalScore = document.createElement("h2");
+    finalScore.textContent = "Congratulations! Your final score is " +score + "!";
+    document.querySelector("#questions").append(finalScore);
+}
+
+
+
+
+// var time = 59;
+// var timeElapsed = 0;
+// // Will be using later . . .
+// var interval;
+
+// function renderTime() {
+//   console.log({timeElapsed, tillEnd: time - timeElapsed});
+// }
+
+// function startTimer() {
+//   renderTime();
+
+//   // setInterval format = setInterval(function(){}, delay);
+//   interval = setInterval(function() {
+//     // Stop condition for the timer 
+//     // (must be within the setInterval)
+//     if (time - timeElapsed <= 0) {
+//       stopTimer();
+//       return;
+//     }
+//     else {
+//       timeElapsed++;
+//     }
+
+//     renderTime();
+//   }, 1000)
+// }
+
+// startTimer();
+
+// function wrongPenalty() {
+//   timeElapsed += 5;
+// }
+
+// function stopTimer() {
+//   console.log("Time's Up!!");
+//   clearInterval(interval);
+// }
+
     //check if counter variable is not equal to questions.length
     //then we will create <p> to add question and loop over options array and create button for each option
     //add data attribute "data-ans" to each button and valur would be each option
@@ -71,62 +146,3 @@ function loadQuiz(num){
     temp.push(uData);
     localstorage.setItem("userData", JSON.stringify(temp));
     */
-
-function ansCheck(){
-    var userAns = this.getAttribute("data-ans");
-    console.log("User Ans: "+userAns);
-    
-    
-    if(userAns === questions[quesCounter].answer){
-        score = score + 10;
-        quesCounter++;
-        loadQuiz(quesCounter);
-    }
-    else{
-        time = time - 5;
-        quesCounter++;
-        loadQuiz(quesCounter);
-    }
-}
-
-/*var time = 59;
-var timeElapsed = 0;
-// Will be using later . . .
-var interval;
-
-function renderTime() {
-  console.log({timeElapsed, tillEnd: time - timeElapsed});
-}
-
-function startTimer() {
-  renderTime();
-
-  // setInterval format = setInterval(function(){}, delay);
-  interval = setInterval(function() {
-    // Stop condition for the timer 
-    // (must be within the setInterval)
-    if (time - timeElapsed <= 0) {
-      stopTimer();
-      return;
-    }
-
-    if (timeElapsed % 9 === 0) { //fakes a wrong answer
-      wrongPenalty();
-    } else {
-      timeElapsed++;
-    }
-
-    renderTime();
-  }, 1000)
-}
-
-startTimer();
-
-function wrongPenalty() {
-  timeElapsed += 5;
-}
-
-function stopTimer() {
-  console.log("Time's Up!!");
-  clearInterval(interval);
-}*/
